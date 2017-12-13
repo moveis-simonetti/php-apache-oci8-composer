0e77bf636d6d199b52acbfcd0c7f736b816f03f8
Vinícius de Sá Souza <viniciusss@me.com>
2017-12-13 10:41:21 -0200

    Update apache-run.sh

--- apache-run.sh
+++ apache-run.sh
@@ -9,4 +9,5 @@ usermod -u 1000 www-data \
     && cd /var/www/html && composer install && rm -rf var/cache/* var/logs/* \
     && chown -R www-data:www-data /var/www/html/var/cache && chmod 777 /var/www/html/var/cache \
     && chown -R www-data:www-data /var/www/html/var/logs && chmod 777 /var/www/html/var/logs \
+    && chown -R www-data:www-data /var/www/html/var/sessions && chmod 777 /var/www/html/var/sessions \
     && apache2-foreground
