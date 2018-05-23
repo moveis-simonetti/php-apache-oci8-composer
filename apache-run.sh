4e8052ca83120800432972de5e6cc9032073b261
Vinicius de Sa Souza <viniciusss@me.com>
2018-05-23 17:21:59 -0300

    remove sudo from apache-run

--- apache-run.sh
+++ apache-run.sh
@@ -9,11 +9,11 @@ fi
 # if the user wants to enable new relic
 if [[ $NR_ENABLED == true ]]; then
 	# set the new relic key
-	sudo sed -i -e "s/"REPLACE_WITH_REAL_KEY"/$NR_LICENSE_KEY/g" /scripts/newrelic.ini
+	sed -i -e "s/"REPLACE_WITH_REAL_KEY"/$NR_LICENSE_KEY/g" /scripts/newrelic.ini
 	# set the new relic application name
-	sudo sed -i -e "s/PHP Application/$NR_APP_NAME/g" /scripts/newrelic.ini
+	sed -i -e "s/PHP Application/$NR_APP_NAME/g" /scripts/newrelic.ini
 	# enable new relic for fpm
-	sudo cp /scripts/newrelic.ini /usr/local/etc/php/conf.d/newrelic.ini
+	cp /scripts/newrelic.ini /usr/local/etc/php/conf.d/newrelic.ini
 fi
 
 usermod -u 1000 www-data \
