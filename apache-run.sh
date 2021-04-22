0ad191db4961d74b9748c5c49aa6f8473122f379
Lucas dos Santos Abreu <lucas.s.abreu@gmail.com>
2021-04-22 09:13:11 -0300

    (chore): termos do xdebug mudaram

--- apache-run.sh
+++ apache-run.sh
@@ -2,19 +2,21 @@
 
 if [[ ${XDEBUG_ENABLED} == true ]]; then
     sudo rm -f /usr/local/etc/php/conf.d/xdebug.ini || true
-    echo "zend_extension=/usr/local/lib/php/extensions/no-debug-non-zts-${PHP_BUILD_DATE}/xdebug.so" | sudo tee -a /usr/local/etc/php/conf.d/xdebug.ini
+    echo "zend_extension=xdebug.so" | sudo tee -a /usr/local/etc/php/conf.d/xdebug.ini
     echo "xdebug.var_display_max_depth=5" | sudo tee -a /usr/local/etc/php/conf.d/xdebug.ini
     echo "xdebug.idekey=${XDEBUG_IDEKEY}" | sudo tee -a /usr/local/etc/php/conf.d/xdebug.ini
-    echo "xdebug.remote_enable=1" | sudo tee -a /usr/local/etc/php/conf.d/xdebug.ini
-    echo "xdebug.remote_port=${XDEBUG_REMOTE_PORT}" | sudo tee -a /usr/local/etc/php/conf.d/xdebug.ini
+    echo "xdebug.mode=debug" | sudo tee -a /usr/local/etc/php/conf.d/xdebug.ini
+    echo "xdebug.client_port=${XDEBUG_REMOTE_PORT:-$XDEBUG_CLIENT_PORT}" | sudo tee -a /usr/local/etc/php/conf.d/xdebug.ini
 
+    export XDEBUG_AUTOSTART=${XDEBUG_AUTOSTART:-$XDEBUG_START_WITH_REQUEST}
     [[ ${XDEBUG_AUTOSTART} == true ]] && {
-        echo "xdebug.remote_autostart=on" | sudo tee -a /usr/local/etc/php/conf.d/xdebug.ini
-    } || echo "xdebug.remote_autostart=off" | sudo tee -a /usr/local/etc/php/conf.d/xdebug.ini
+        echo "xdebug.start_with_request=yes" | sudo tee -a /usr/local/etc/php/conf.d/xdebug.ini
+    } || echo "xdebug.start_with_request=no" | sudo tee -a /usr/local/etc/php/conf.d/xdebug.ini
 
+    export XDEBUG_CONNECT_BACK=${XDEBUG_CONNECT_BACK:-$XDEBUG_DISCOVER_CLIENT_HOST}
     [[ ${XDEBUG_CONNECT_BACK} == true ]] && {
-        echo "xdebug.remote_connect_back=1" | sudo tee -a /usr/local/etc/php/conf.d/xdebug.ini
-    } || echo "xdebug.remote_connect_back=0" | sudo tee -a /usr/local/etc/php/conf.d/xdebug.ini
+        echo "xdebug.discover_client_host=1" | sudo tee -a /usr/local/etc/php/conf.d/xdebug.ini
+    } || echo "xdebug.discover_client_host=0" | sudo tee -a /usr/local/etc/php/conf.d/xdebug.ini
 fi
 
 if [[ ${NR_ENABLED} == true ]]; then
